370f7cf9752ab5c158e57063d2f2b03fbc492b6f
Rahn20 <ranimhanna@hotmail.com>
2023-01-08 17:31:07 +0000

    Created getAllCustomers function

--- src/models/customer.js
+++ src/models/customer.js
@@ -1,16 +1,12 @@
-/**
- * 
- * 
-*/
-
-
 "use strict";
 
 import m from 'mithril';
 
-let customer = {
-    url: "",
-    customerData: [],
+
+const Customer = {
+    url: "http://localhost:1337/api/v1/graphql",
+    res: "",
+    allCustomers: [],
 
     getCustomer: function() {
         // add to customerData list
@@ -18,7 +14,34 @@ let customer = {
 
     updateCustomer: function() {
     },
-}
+
+    // Get all customers and add customer data to the allCustomers list.
+    getAllCustomers: async () => {
+        const query = `
+            query {
+                getAllCustomers {
+                    user {
+                        username
+                        email
+                    }
+
+                }
+            }
+      `;
+
+        try {
+            const result = await m.request({
+                method: "POST",
+                url: Customer.url,
+                body: { query }
+            });
+
+            Customer.allCustomers = result.data.getAllCustomers;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+};
 
 
-export { customer };
+export { Customer };
