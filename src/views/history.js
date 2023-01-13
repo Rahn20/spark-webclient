bfa06b909a6115583600d6659d0e0aff2faacf20
Rahn20 <ranimhanna@hotmail.com>
2023-01-13 07:34:26 +0000

    Added table to view the history information

--- src/views/history.js
+++ src/views/history.js
@@ -4,19 +4,51 @@
 
 "use strict";
 
-import m from "mithril";
+const m = require('mithril');
+const Historys = require('./../models/history');
+
 
 const History = {
-    oninit: function() {
-        // load user's history -> Models
+    oninit: () => {
+        Historys.getHistory();
     },
 
-    view: function () {
-        // show user's history
-        return m("h2", "Historik över resorna");
+    view: () => {
+        let count = 1;
+
+        return [
+            m("h2", "Kund Historik"),
+            m("hr"),
+
+            m("table.table table-hover", [
+                m("thead", [
+                    m("tr.table-primary", [
+                        m("th", { scope: "col" }, "Nr"),
+                        m("th", { scope: "col" }, "Sparkcykel id"),
+                        m("th", { scope: "col" }, "Starttid"),
+                        m("th", { scope: "col" }, "Sluttid"),
+                        m("th", { scope: "col" }, "Start position"),
+                        m("th", { scope: "col" }, "Slut position")
+                    ]),
+                ]),
+
+                m("tbody", [
+                    Historys.historyData.map((res) => {
+                        return m("tr.table-active", [
+                            m("td", count++),
+                            m("td", res.scooter_id),
+                            m("td", res.start_time),
+                            m("td", res.end_time),
+                            m("td", res.start_latitude + ", " + res.start_longitude),
+                            m("td", res.end_latitude + ", " + res.end_longitude),
+                        ]);
+                    })
+                ])
+            ])
+        ];
     },
 
 };
 
 
-export { History };
+module.exports = History;
