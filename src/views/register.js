a69c9707b339552119d5905b8531cb814b4b19db
Rahn20 <ranimhanna@hotmail.com>
2023-01-13 07:53:42 +0000

    Created register form

--- src/views/register.js
+++ src/views/register.js
@@ -5,9 +5,9 @@
 "use strict";
 
 
-import m from "mithril";
-import { Auth } from "../models/auth";
-import { Customer } from "../models/customer";
+const m = require('mithril');
+const Auth = require('../models/auth');
+const Customer = require('../models/customer');
 
 
 const Register = {
@@ -15,9 +15,11 @@ const Register = {
         Customer.getAllCustomers();
     },
 
-    view: function () {
-        return [
+    view: () => {
+        return m("div#register_page", [
             m("h2", "Registrera dig här"),
+            m("hr"),
+
             m("form.register", {
                 onsubmit: function(event) {
                     event.preventDefault();
@@ -25,63 +27,60 @@ const Register = {
                 }
             },
             [
-                m("p", [
-                    m("label.label", "Användarnamn"),
-                    m("input.input[type=text][required]", {
-                        oninput: (event) => {
-                            Auth.registerData.username = event.target.value;
-                        }, value: Auth.registerData.username
-                    })
-                ]),
+                m("p", m("label.label", "Användarnamn")),
+                m("input.input[type=text][required]", {
+                    oninput: (event) => {
+                        Auth.registerData.username = event.target.value;
+                    }, value: Auth.registerData.username
+                }),
 
-                m("p", [
-                    m("label.label", "Förnamn"),
-                    m("input.input[type=text][required]", {
-                        oninput: (event) => {
-                            Auth.registerData.firstName = event.target.value;
-                        }, value: Auth.registerData.firstName
-                    }),
 
-                    m("label.label", "Efternamn"),
-                    m("input.input[type=text][required]", {
-                        oninput: (event) => {
-                            Auth.registerData.lastName = event.target.value;
-                        }, value: Auth.registerData.lastName
-                    })
-                ]),
+                m("p", m("label.label", "Förnamn")),
+                m("input.input[type=text][required]", {
+                    oninput: (event) => {
+                        Auth.registerData.firstName = event.target.value;
+                    }, value: Auth.registerData.firstName
+                }),
 
-                m("p", [
-                    m("label.label", "Email"),
-                    m("input.input[type=email][required]", {
-                        oninput: (event) => {
-                            Auth.registerData.email = event.target.value;
-                        }, value: Auth.registerData.email
-                    }),
+                m("p", m("label.label", "Efternamn")),
+                m("input.input[type=text][required]", {
+                    oninput: (event) => {
+                        Auth.registerData.lastName = event.target.value;
+                    }, value: Auth.registerData.lastName
+                }),
 
-                    m("label.label", "Mobilnummer"),
-                    m("input.input[type=text][required]", {
-                        oninput: (event) => {
-                            Auth.registerData.phone = event.target.value;
-                        }, value: Auth.registerData.phone
-                    })
-                ]),
 
-                m("p", [
-                    m("label.label", "lösenord"),
-                    m("input.input[type=password][required]", {
-                        oninput: (event) => {
-                            Auth.registerData.password = event.target.value;
-                        }, value: Auth.registerData.password
-                    })
-                ]),
 
-                m("div#register", m("button.button[type=submit]", "Registrera")),
+                m("p", m("label.label", "Email")),
+                m("input.input[type=email][required]", {
+                    oninput: (event) => {
+                        Auth.registerData.email = event.target.value;
+                    }, value: Auth.registerData.email
+                }),
+
+                m("p", m("label.label", "Mobilnummer")),
+                m("input.input[type=tel][required][pattern='[0-9]{10}'][placeholder=0712345678]", {
+                    oninput: (event) => {
+                        Auth.registerData.phone = event.target.value;
+                    }, value: Auth.registerData.phone
+                }),
+
+                m("p", m("label.label", "lösenord")),
+                m("input.input[type=password][required]", {
+                    oninput: (event) => {
+                        Auth.registerData.password = event.target.value;
+                    }, value: Auth.registerData.password
+                }),
+
+
+
+                m("hr"),
+                m("div#register", m("button.registerbtn[type=submit]", "Registrera")),
             ]),
 
-            m("p", Auth.res)
-        ];
+            m("p", Auth.res),
+        ]);
     }
 };
 
-
-export { Register };
+module.exports = Register;
