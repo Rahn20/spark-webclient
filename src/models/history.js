330a62641a690c2466d5333d219f1ba08b0597e8
Rahn20 <ranimhanna@hotmail.com>
2023-01-13 07:17:23 +0000

    Created gethistory function

--- src/models/history.js
+++ src/models/history.js
@@ -1,16 +1,49 @@
 "use strict";
 
-//import m from 'mithril';
+const m = require('mithril');
+const Customer = require('./customer');
 
-const History = {
-    url: "",
+
+const Historys = {
+    url: "http://localhost:1337/api/v1/graphql",
     historyData: [],
 
-    getHistory: function() {
-        // get customer's trip history
-        // add to list
+    // get customer log
+    getHistory: async () => {
+        const query = `
+            query($customerId: String!) {
+                getLogByCustomerId(customerId: $customerId) {
+                    customer_name
+                    scooter_id
+                    customer_id
+                    start_time
+                    end_time
+                    start_latitude
+                    start_longitude
+                    end_latitude
+                    end_longitude  
+                }
+            }
+        `;
+
+        const variables = {
+            customerId: Customer.customer.id
+        };
+
+
+        try {
+            const result = await m.request({
+                method: "POST",
+                url: Historys.url,
+                body: { query, variables}
+            });
+
+            Historys.historyData = result.data.getLogByCustomerId;
+        } catch (e) {
+            console.log(e);
+        }
     },
 };
 
 
-export { History };
+module.exports = Historys;
