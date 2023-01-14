2c5aac7ae015c2e72e034e02a24b983410f345e8
Rahn20 <ranimhanna@hotmail.com>
2023-01-14 06:04:39 +0000

    Added headers/plugins

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,54 +2,63 @@
  * Configuration file for development
  */
 
-const path = require('path');
-//const webpack = require('webpack');
+
+const path = require("path");
+const dotenv = require("dotenv-webpack");
 
 const port = 1338;
 const openBrowser = false;
-
 
 const rules = [
     {
         test: /\.css$/,
         use: [
-            'style-loader',
-            'css-loader'
+            "style-loader",
+            "css-loader"
         ]
     },
     {
         test: /\.(png|svg|jpg|ico|gif)$/,
         use: [
             {
-                loader: 'file-loader',
+                loader: "file-loader",
                 options: {
-                    name: '[name].[ext]',
+                    name: "[name].[ext]",
                 },
             },
         ]
-    }
+    },
 ];
 
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'source-map',
+    mode: "development",
+    entry: {
+        filename: "./src/index.js",
+    },
+    devtool: "source-map",
     output: {
         filename: "dist/bundle.js",
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, "build"),
         publicPath: "/",
     },
     module: {
         rules: rules
     },
 
+    plugins: [ new dotenv() ],
+
     devServer: {
+
+        headers: {
+            // eslint-disable-next-line max-len
+            "Content-Security-Policy": "default-src 'self' http://localhost:1337; connect-src 'self' http://localhost:1337;  font-src *; style-src *;",
+        },
         port: port,
         open: openBrowser,
-        host: 'localhost',
+        host: "localhost",
         historyApiFallback: {
-            index: 'index.html',
+            index: "index.html",
         },
         static: {
             directory: path.join(__dirname, 'public'),
