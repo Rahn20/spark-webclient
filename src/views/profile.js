92f5f11f62a59e407b7e270bc83ffddce07d9089
Rahn20 <ranimhanna@hotmail.com>
2023-01-18 15:02:40 +0000

    Removed if-statement

--- src/views/profile.js
+++ src/views/profile.js
@@ -6,45 +6,14 @@
 
 const m = require('mithril');
 const Customer = require('../models/customer');
-const Auth = require('./../models/auth');
 
 
 const Profile = {
     oninit: () => {
-        if (Auth.user.email) {
-            Customer.getCustomerAccount();
-        }
+        Customer.getCustomerAccount();
     },
 
     view: () => {
-        if (Customer.customer.username) {
-            return [
-                m("h2", "Kund profil"),
-                m("hr"),
-
-                m("div#profile", [
-                    m("div.circle-border", m("p", "profil")),
-
-                    m("div.profile", [
-                        m("ul", [
-                            m("li", m("strong", "Användarnamn: "), Customer.customer.username),
-                            m("li", m("strong", "Namn: "), Customer.customer.first_name + " " + Customer.customer.last_name),
-                            m("li", m("strong", "Mailadress: "),  Customer.customer.email),
-                            m("li", m("strong", "Mobilnummer: "), Customer.customer.phone),
-                        ]),
-
-                        m("div.account", [
-                            m("h4", "Konto"),
-                            m("ul", [
-                                m("li", m("strong", "Betalningsmetod: "), Customer.account.payment_method),
-                                m("li", m("strong", "Saldot på konto: "), Customer.account.balance + " Kr")
-                            ])
-                        ])
-                    ])
-                ])
-            ];
-        }
-
         return [
             m("h2", "Kund profil"),
             m("hr"),
@@ -54,15 +23,18 @@ const Profile = {
 
                 m("div.profile", [
                     m("ul", [
-                        m("li", m("strong", "Användarnamn: "), Auth.oauth.username),
-                        m("li", m("strong", "Namn: "), Auth.oauth.first_name + " " + Auth.oauth.last_name),
-                        m("li", m("strong", "Mailadress: "),  Auth.oauth.email),
-                        m("li", m("strong", "Mobilnummer: "), Auth.oauth.phone),
+                        m("li", m("strong", "Användarnamn: "), Customer.customer.username),
+                        m("li", m("strong", "Namn: "), Customer.customer.first_name + " " + Customer.customer.last_name),
+                        m("li", m("strong", "Mailadress: "),  Customer.customer.email),
+                        m("li", m("strong", "Mobilnummer: "), Customer.customer.phone),
                     ]),
 
                     m("div.account", [
                         m("h4", "Konto"),
-                        m("p", "Det finns inget konto att visa."),
+                        m("ul", [
+                            m("li", m("strong", "Betalningsmetod: "), Customer.account.payment_method),
+                            m("li", m("strong", "Saldot på konto: "), Customer.account.balance + " Kr")
+                        ])
                     ])
                 ])
             ])
