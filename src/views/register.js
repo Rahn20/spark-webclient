281f771872cce9644da7bf437e2858629ef5a113
Rahn20 <ranimhanna@hotmail.com>
2023-01-21 03:28:11 +0000

    Updated register form

--- src/views/register.js
+++ src/views/register.js
@@ -7,13 +7,11 @@
 
 const m = require('mithril');
 const Auth = require('../models/auth');
-const Customer = require('../models/customer');
 
 
 const Register = {
     oninit: () => {
         Auth.res = "";
-        Customer.getAllCustomers();
     },
 
     view: () => {
@@ -22,14 +20,19 @@ const Register = {
             m("hr"),
 
             m("form.register", {
-                onsubmit: function(event) {
+                onsubmit: async (event) => {
                     event.preventDefault();
-                    Auth.register(Customer.allCustomers);
+                    let result = await Auth.checkUser();
+
+                    // Register customer if customer is not already registered.
+                    if (result === true) {
+                        Auth.register();
+                    }
                 }
             },
             [
                 m("p", m("label.label", "Användarnamn")),
-                m("input.input[type=text][required]", {
+                m("input.input[type=text][name=username][required]", {
                     oninput: (event) => {
                         Auth.registerData.username = event.target.value;
                     }, value: Auth.registerData.username
@@ -37,37 +40,36 @@ const Register = {
 
 
                 m("p", m("label.label", "Förnamn")),
-                m("input.input[type=text][required]", {
+                m("input.input[type=text][name=firstname][required]", {
                     oninput: (event) => {
                         Auth.registerData.firstName = event.target.value;
                     }, value: Auth.registerData.firstName
                 }),
 
                 m("p", m("label.label", "Efternamn")),
-                m("input.input[type=text][required]", {
+                m("input.input[type=text][name=lastname][required]", {
                     oninput: (event) => {
                         Auth.registerData.lastName = event.target.value;
                     }, value: Auth.registerData.lastName
                 }),
 
 
-
                 m("p", m("label.label", "Email")),
-                m("input.input[type=email][required]", {
+                m("input.input[type=email][name=email][required]", {
                     oninput: (event) => {
                         Auth.registerData.email = event.target.value;
                     }, value: Auth.registerData.email
                 }),
 
                 m("p", m("label.label", "Mobilnummer")),
-                m("input.input[type=tel][required][pattern='[0-9]{10}'][placeholder=0712345678]", {
+                m("input.input[type=tel][required][name=tel][pattern='[0-9]{10}'][placeholder=0712345678]", {
                     oninput: (event) => {
                         Auth.registerData.phone = event.target.value;
                     }, value: Auth.registerData.phone
                 }),
 
                 m("p", m("label.label", "lösenord")),
-                m("input.input[type=password][required]", {
+                m("input.input[type=password][name=password][required]", {
                     oninput: (event) => {
                         Auth.registerData.password = event.target.value;
                     }, value: Auth.registerData.password
