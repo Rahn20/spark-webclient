9eb27a73f350a50381d8fd53876d4c7e433cf001
Rahn20 <ranimhanna@hotmail.com>
2023-01-21 03:25:09 +0000

    Updated checkUser

--- src/models/auth.js
+++ src/models/auth.js
@@ -11,12 +11,13 @@ const Auth = {
 
     // users data
     user: {
+        id: 0,
         email: "",
         password: "",
     },
 
 
-    // Returns true when user succeed with login, otherwise returns false
+    // Returns true when user succeed with login
     login: async () => {
         try {
             const result = await m.request({
@@ -34,10 +35,10 @@ const Auth = {
 
             if (result.user.token) {
                 Auth.user.password = "";
+                Auth.user.id = result.user.id;
                 Auth.isLogin = true;
                 return true;
             }
-            return false;
         } catch (e) {
             Auth.res = "Något gick fel! antigen lösenordet eller mailadressen är fel.";
             console.log(e);
@@ -46,39 +47,34 @@ const Auth = {
 
 
     // register new customer
-    register: async (customers) => {
-        let user = Auth.checkUser(customers);
+    register: async () => {
+        try {
+            await m.request({
+                method: "POST",
+                url: `${Auth.url}/auth/register`,
+                body: {
+                    username: Auth.registerData["username"],
+                    password: Auth.registerData["password"],
+                    phone: Auth.registerData["phone"],
+                    email: Auth.registerData["email"],
+                    firstName: Auth.registerData["firstName"],
+                    lastName: Auth.registerData["lastName"],
+                    roleId: "2",
+                },
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
 
-        if (user) {
-            Auth.res = "Användarnamnet eller email adressen används redan.";
-        } else {
-            try {
-                await m.request({
-                    method: "POST",
-                    url: `${Auth.url}/auth/register`,
-                    body: {
-                        username: Auth.registerData["username"],
-                        password: Auth.registerData["password"],
-                        phone: Auth.registerData["phone"],
-                        email: Auth.registerData["email"],
-                        firstName: Auth.registerData["firstName"],
-                        lastName: Auth.registerData["lastName"],
-                        roleId: "2",
-                    },
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                });
-
-                Auth.registerData = [];
-                return m.route.set("/");
-            } catch (e) {
-                console.log(e);
-            }
+            Auth.registerData = [];
+            return m.route.set("/");
+        } catch (e) {
+            console.log(e);
         }
     },
 
 
+    // login with google
     loginWithGoogle: async () => {
         window.location.href = `${Auth.url}/auth/google`;
         localStorage.setItem("oauth", true);
@@ -99,6 +95,7 @@ const Auth = {
 
             if (res.status === 200 && res.user) {
                 Auth.user.email = res.user.email;
+                Auth.user.id = res.user.id;
                 Auth.isLogin = true;
             }
         } catch (e) {
@@ -106,24 +103,40 @@ const Auth = {
         }
     },
 
-
-    // Returns true if the username or email address exists.
-    checkUser: (customers) => {
-        let email = Auth.registerData["email"];
-        let username = Auth.registerData["username"];
-        let res = false;
-
-        for (let i = 0; i < customers.length; i++) {
-            if (
-                customers[i].user.username == username ||
-                customers[i].user.email == email
-            ) {
-                res = true;
-                break;
+    // Returns true if the username or email address does not exists, otherwise return true
+    checkUser: async () => {
+        const query = `
+            query($username: String!, $email: String!) {
+                getUserByUsernameOrEmail(username: $username, email: $email) {
+                    id
+                }
             }
-        }
+        `;
 
-        return res;
+        const variables = {
+            username: Auth.registerData["username"],
+            email: Auth.registerData["email"]
+        };
+
+        try {
+            const result = await m.request({
+                method: "POST",
+                url: `${Auth.url}/graphql`,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: { query: query, variables }
+            });
+
+
+            if (result.data.getUserByUsernameOrEmail.length === 0) {
+                return true;
+            } else {
+                Auth.res = "Användarnamnet eller email adressen används redan.";
+            }
+        } catch (e) {
+            console.log(e);
+        }
     },
 };
 
